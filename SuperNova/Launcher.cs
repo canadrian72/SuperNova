564f702ad0aa37ad03281d2d2be1633cc4b01150
canadrian72 <patterson.n.adrian@gmail.com>
2021-03-07 17:43:34 -0500

    Trying new PWM things -- Trial and Error at this point

--- SuperNova/Launcher.cs
+++ SuperNova/Launcher.cs
@@ -3,6 +3,7 @@ using Unosquare.RaspberryIO;
 using Unosquare.RaspberryIO.Abstractions;
 using Unosquare.WiringPi;
 
+
 namespace SuperNova
 {
     public class Launcher
@@ -10,53 +11,20 @@ namespace SuperNova
 
         public void launchTreat()
         {
-            var pin = (GpioPin)Pi.Gpio[BcmPin.Gpio23];
+            Pi.Init<BootstrapWiringPi>();
+
+            var pin = (GpioPin)Pi.Gpio[BcmPin.Gpio18];
             pin.PinMode = GpioPinDriveMode.PwmOutput;
             pin.PwmMode = PwmMode.Balanced;
             pin.PwmClockDivisor = 2;
-            while (true)
-            {
-                for (var x = 0; x <= 100; x++)
-                {
-                    pin.PwmRegister = (int)pin.PwmRange / 100 * x;
-                    Thread.Sleep(10);
-                }
 
-                for (var x = 0; x <= 100; x++)
-                {
-                    pin.PwmRegister = (int)pin.PwmRange - ((int)pin.PwmRange / 100 * x);
-                    Thread.Sleep(10);
-                }
-            }
+
+            pin.PwmRegister = 1024;
+            Thread.Sleep(5000);
+           
+            
+            pin.PwmRegister = 0;
+            Thread.Sleep(5000);
         }
-        //public string launch { get; set; }
-        //public GpioPin pin;
-        //public int range;
-
-        //public Launcher()
-        //{
-        //    Pi.Init<BootstrapWiringPi>();
-
-        //    range = 360;
-        //    pin = (GpioPin)Pi.Gpio[BcmPin.Gpio23];
-        //    pin.PinMode = GpioPinDriveMode.Output;
-        //    pin.StartSoftPwm(0, range);
-        //}
-
-        //public void launchTreat()
-        //{
-        //    for (var x = 0; x <= 100; x++)
-        //    {
-        //        pin.SoftPwmValue = range / 100 * x;
-        //        Thread.Sleep(10);
-        //    }
-
-        //    for (var x = 0; x <= 100; x++)
-        //    {
-        //        pin.SoftPwmValue = range - (range / 100 * x);
-        //        Thread.Sleep(10);
-        //    }
-        //}
-        
     }
 }
