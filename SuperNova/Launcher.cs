450afbc535a55da3ad3b22ad2d3a34e0d23291e9
canadrian72 <patterson.n.adrian@gmail.com>
2021-03-07 15:26:41 -0500

    Going to use an arduino for servo control

--- SuperNova/Launcher.cs
+++ SuperNova/Launcher.cs
@@ -1,30 +1,25 @@
-﻿using System.Threading;
-using Unosquare.RaspberryIO;
-using Unosquare.RaspberryIO.Abstractions;
-using Unosquare.WiringPi;
+﻿using System;
+using System.Threading;
+using System.Device.Gpio;
 
 
 namespace SuperNova
 {
     public class Launcher
     {
+        GpioController controller = new GpioController();
+        private static readonly int pin = 23;
+
+        public Launcher()
+        {
+            controller.OpenPin(pin, PinMode.Output);
+        }
 
         public void launchTreat()
         {
-            Pi.Init<BootstrapWiringPi>();
-
-            var pin = (GpioPin)Pi.Gpio[BcmPin.Gpio18];
-            pin.PinMode = GpioPinDriveMode.PwmOutput;
-            pin.PwmMode = PwmMode.Balanced;
-            pin.PwmClockDivisor = 2;
-
-
-            pin.PwmRegister = 1024;
-            Thread.Sleep(5000);
-           
-            
-            pin.PwmRegister = 0;
-            Thread.Sleep(5000);
+            controller.Write(pin, PinValue.High);
+            Thread.Sleep(100);
+            controller.Write(pin, PinValue.Low);
         }
     }
 }
